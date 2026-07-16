ac501951f4294aa1d4a1d5233dc09d1c741b892f
SG-1022 <spg222020@gmail.com>
2026-07-16 13:16:13 -0500

    Refactor birthday email notification logic
    Refactored birthday email notification code to use environment variables for email credentials and improved structure.

--- main.py
+++ main.py
@@ -1,38 +1,51 @@
-# To run and test the code you need to update 4 places:
-# 1. Change MY_EMAIL/MY_PASSWORD to your own details.
-# 2. Go to your email provider and make it allow less secure apps.
-# 3. Update the SMTP ADDRESS to match your email provider.
-# 4. Update birthdays.csv to contain today's month and day.
-# See the solution video in the 100 Days of Python Course for explainations.
-
-
-from datetime import datetime
-import pandas
-import random
-import smtplib
 import os
+from dotenv import load_dotenv
+import smtplib
+import datetime
 
-# import os and use it to get the Github repository secrets
-MY_EMAIL = os.environ.get("MY_EMAIL")
-MY_PASSWORD = os.environ.get("MY_PASSWORD")
+today = datetime.date.today()
+today = (today.month, today.day)
 
-today = datetime.now()
-today_tuple = (today.month, today.day)
+mom = (12, 25)
+dad = (9, 28)
+me = (7, 16)
+bro = (1, 19)
 
-data = pandas.read_csv("birthdays.csv")
-birthdays_dict = {(data_row["month"], data_row["day"])                  : data_row for (index, data_row) in data.iterrows()}
-if today_tuple in birthdays_dict:
-    birthday_person = birthdays_dict[today_tuple]
-    file_path = f"letter_templates/letter_{random.randint(1, 3)}.txt"
-    with open(file_path) as letter_file:
-        contents = letter_file.read()
-        contents = contents.replace("[NAME]", birthday_person["name"])
+info = {
+    'mom': (mom, "rohinipgurav@gmail.com"),
+    'dad': (dad, "pareshgurav@gmail.com"),
+    'me': (me, "sgpython452@gmail.com"),
+    'bro': (bro, "spg192020@gmail.com"),
+}
 
-    with smtplib.SMTP("YOUR EMAIL PROVIDER SMTP SERVER ADDRESS") as connection:
+for name in info:
+    if info[name][0] == today:
+        h = True
+        name = name
+        PERSON_EMAIL = info[name][1]
+        break
+else:
+    h = False
+
+if h:
+
+    load_dotenv()
+
+    EMAIL = os.environ.get("EMAIL")  # Do not use child restricte account.
+    PASSWORD = os.environ.get("PASSWORD")
+
+    if not EMAIL or not PASSWORD:
+        raise ValueError("EMAIL or PASSWORD not found in .env")
+
+    with smtplib.SMTP("smtp.gmail.com", 587) as connection:
         connection.starttls()
-        connection.login(MY_EMAIL, MY_PASSWORD)
-        connection.sendmail(
-            from_addr=MY_EMAIL,
-            to_addrs=birthday_person["email"],
-            msg=f"Subject:Happy Birthday!\n\n{contents}"
-        )
+        connection.login(EMAIL, PASSWORD)
+
+        subject = "Happy Birthday {}".format(name)
+        body = "Happy Birthday {}, hope you have fun today.".format(name)
+
+        message = f"Subject:{subject}\n\n{body}\nThis is sent from python.\n\nFrom\nSiddhaanth Gurav"
+        connection.sendmail(from_addr=EMAIL, to_addrs=PERSON_EMAIL, msg=message)
+    print("Today is {}'s birthday and msg is sent. \n{}".format(name, body))
+else:
+    print("Today is no one's birthday...")
